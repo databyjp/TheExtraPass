92119567d419d323a95994f9a6f0f6bb9afc1a5b
JP Hwang <me@jphwang.com>
2022-02-10 11:13:45 +0800

    ran black on the remainder

--- nba_api_test_shotchart.py
+++ nba_api_test_shotchart.py
@@ -3,22 +3,19 @@ from nba_api.stats.endpoints import shotchartdetail
 import json
 
 response = shotchartdetail.ShotChartDetail(
-	team_id=0,
-	player_id=201935,
-	season_nullable='2018-19',
-	date_from_nullable=20181210,
-	date_to_nullable=20181201,
-	season_type_all_star='Regular Season'
+    team_id=0,
+    player_id=201935,
+    season_nullable="2018-19",
+    date_from_nullable=20181210,
+    date_to_nullable=20181201,
+    season_type_all_star="Regular Season",
 )
 
 content = json.loads(response.get_json())
 
 # transform contents into dataframe
-results = content['resultSets'][0]
-headers = results['headers']
-rows = results['rowSet']
+results = content["resultSets"][0]
+headers = results["headers"]
+rows = results["rowSet"]
 df = pd.DataFrame(rows)
 df.columns = headers
-
-
-
