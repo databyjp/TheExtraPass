c70388b93279334162206ce3e59c5f150f2dd070
JP Hwang <me@jphwang.com>
2022-02-17 14:48:44 +0800

    Updated to match blogpost

--- update_data.py
+++ update_data.py
@@ -199,7 +199,7 @@ def main():
 
     # update_season_pl_list(2015, 2021)
     # update_season_pl_gamelogs()
-    update_box_scores()
+    update_box_scores()  # TODO - need a faster way of fetching a list of all games
 
 
 if __name__ == "__main__":
