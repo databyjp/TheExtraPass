7c8854824363512bd78e6233843d02c19db6cee1
JP Hwang <me@jphwang.com>
2022-03-17 22:24:21 +0700

    Added chart w moving window

--- 20220316_pbp_shot_dists.py
+++ 20220316_pbp_shot_dists.py
@@ -4,9 +4,9 @@ import logging
 import pandas as pd
 import numpy as np
 import utils
-from nba_api.stats.static import players
 from nba_api.stats.static import teams
 import plotly.express as px
+import re
 
 logger = logging.getLogger(__name__)
 root_logger = logging.getLogger()
@@ -82,4 +82,72 @@ fig = px.bar(gdf_tot, x="team", facet_row="shot_zone", y="shot_freq", color="rel
                  color_continuous_scale=px.colors.diverging.RdYlBu_r, color_continuous_midpoint=0,
                  category_orders={"team": tm_ranks})
 fig.update_traces(marker=dict(line=dict(width=1, color='DarkSlateGrey')))
+fig.show()
+
+
+def get_shot_dist(df_in, grp_label):
+    # Build more granular shot bins
+    filt_start = 0
+    filt_width = 2
+    filt_inc = 0.25
+    ser_list = list()
+    for i in range(1 + 120):
+        for shot_type in ["2pt", "3pt"]:
+            filt_df = df_in[
+                (df_in["shotDistance"] >= filt_start) &
+                (df_in["shotDistance"] < (filt_start + filt_width)) &
+                (df_in["actionType"] == shot_type)
+                ]
+            if len(filt_df) > 0:
+                ser = pd.Series({"shot_made": filt_df["shot_made"].sum(), "shot_atts": len(filt_df)})
+                ser["group"] = grp_label
+                ser["shot_type"] = shot_type
+                ser["filt_start"] = filt_start
+                ser["shot_freq"] = ser.shot_atts / len(df_in)
+                ser["shot_acc"] = ser.shot_made / ser.shot_atts
+                ser_list.append(ser)
+        filt_start += filt_inc
+    df_out = pd.DataFrame(ser_list)
+    return df_out
+
+
+gdf = get_shot_dist(shots_df, "NBA")
+fig = px.scatter(gdf, x="filt_start", y="shot_freq", color="shot_type")
+fig.show()
+
+# Do the same with team data also
+tm_gdfs = list()
+for tm_id in shots_df.teamId.unique():
+    tm_df = shots_df[shots_df.teamId == tm_id]
+    tm_name = teams.find_team_name_by_id(tm_id)["abbreviation"]
+    tm_gdf = get_shot_dist(tm_df, tm_name)
+    # Set relative freqs
+    tm_gdf = tm_gdf.merge(gdf[["shot_type", "filt_start", "shot_freq", "shot_acc"]], how="inner", on=["shot_type", "filt_start"])
+    tm_gdf = tm_gdf.assign(rel_freq=tm_gdf.shot_freq_x - tm_gdf.shot_freq_y)
+    tm_gdf = tm_gdf.assign(rel_acc=tm_gdf.shot_acc_x - tm_gdf.shot_acc_y)
+    tm_gdfs.append(tm_gdf)
+
+all_gdfs = pd.concat(tm_gdfs)
+
+fig = px.scatter(all_gdfs, y="filt_start", x="group", color="rel_freq", facet_row="shot_type",
+                 title="Relative shot frequencies by distance",
+                 color_continuous_scale=px.colors.diverging.RdYlBu_r,
+                 template="plotly_white",
+                 category_orders={"group": tm_ranks})
+for k in fig.layout:
+    if re.search('yaxis[1-9]+', k):
+        fig.layout[k].update(matches=None)
+fig.show()
+
+all_gdfs = all_gdfs.assign(rel_pts=all_gdfs.rel_acc * all_gdfs.shot_freq_x * 2)
+all_gdfs.loc[all_gdfs["shot_type"] == "3pt", "rel_pts"] = all_gdfs[all_gdfs["shot_type"] == "3pt"]["rel_pts"] * 1.5
+
+fig = px.scatter(all_gdfs, y="filt_start", x="group", color="rel_pts", facet_row="shot_type",
+                 title="Where each team gains/loses points vs others",
+                 color_continuous_scale=px.colors.diverging.RdYlBu_r,
+                 template="plotly_white",
+                 category_orders={"group": tm_ranks})
+for k in fig.layout:
+    if re.search('yaxis[1-9]+', k):
+        fig.layout[k].update(matches=None)
 fig.show()
